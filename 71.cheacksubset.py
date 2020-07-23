a750f00be8ac9d3dbc41bcfac107aca5f72394cd
jaydeep <jaydeeppatidar87@gmail.com>
2020-07-23 12:16:46 +0530

    check subset

--- 71.cheacksubset.py
+++ 71.cheacksubset.py
@@ -1,3 +1,4 @@
+
 print("program to check whether a given set is a subset of another given set")
 s1=set([eval(x) for x in input('enter elements in superset set 1 as it is as you want to see in your set with separator space\n').split()])
 s2=set([eval(x) for x in input('enter elements subset set 2 as it is as you want to see in your set with separator space\n').split()])
