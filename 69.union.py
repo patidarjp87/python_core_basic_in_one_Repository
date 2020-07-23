a0e839c1cdfe6bb7c256f2899502d8f3b36e4e1a
jaydeep <jaydeeppatidar87@gmail.com>
2020-07-23 12:15:10 +0530

    union

--- 69.union.py
+++ 69.union.py
@@ -1,3 +1,4 @@
+
 print("program to find union bw two sets")
 s1=set([eval(x) for x in input('enter elements for set 1 as it is as you want to see in your set with separator space\n').split()])
 s2=set([eval(x) for x in input('enter elements for set 2 as it is as you want to see in your set with separator space\n').split()])
