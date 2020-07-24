728bbcaa2a3572570958f7e7f7c507219a45385e
jaydeep <jaydeeppatidar87@gmail.com>
2020-07-24 22:41:31 +0530

    sort according to value

--- 79.sortaccordingvalue.py
+++ 79.sortaccordingvalue.py
@@ -1,3 +1,4 @@
+
 print(" script to sort dict according to value")
 n=int(input('Enter ....how many pairs of key:value do you want to in your dict...?'))
 d={eval(input('key')):eval(input('value')) for x in range(n)}
