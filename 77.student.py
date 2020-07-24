18142cd2d2a1d38726db1c8d5f703295ee06e2e1
jaydeep <jaydeeppatidar87@gmail.com>
2020-07-24 22:40:23 +0530

    basicsof dict 3

--- 77.student.py
+++ 77.student.py
@@ -1,3 +1,4 @@
+
 print("script to create dict in which student roll n0. as key and name as value")
 n=int(input("Enter.....how many students roll no or name do you want to add in dict...?\n"))
 d={eval(input('student roll no. ')):eval(input(' student name in double/single quotes ')) for x in range(n)}
