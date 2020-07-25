4a7005cd5bc8627650625ae1363edf741537f569
jaydeep <jaydeeppatidar87@gmail.com>
2020-07-25 22:40:46 +0530

    basics of function

--- 87.program.py
+++ 87.program.py
@@ -1,3 +1,4 @@
+
 print("script to write a function which takes a list of strings as an arguments and returns a dict whose each key is a alphabet and list of strings begin with that alphabet")
 l=eval(input('Enter a list'))
 def fun(l):
