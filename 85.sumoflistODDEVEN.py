79a61e27fdf8f07f1938f51d1e393165cf2b507c
jaydeep <jaydeeppatidar87@gmail.com>
2020-07-25 22:39:46 +0530

    sum of list elements separated odd even

--- 85.sumoflistODDEVEN.py
+++ 85.sumoflistODDEVEN.py
@@ -1,3 +1,4 @@
+
 print("script to sum of all even no. and sum of all odd no. from a given list\nTakes something returns something")
 l=[eval(x) for x in input('Enter elements of list as it as you want to see in list by separator space').split()]
 def sum(l):
